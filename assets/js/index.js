02f08eb108b821eec2600ad6b752aa3588c7ee89
Yves91700 <yvespierre91700@gmail.com>
2022-10-09 21:44:00 +0200

    ajustement

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,7 +8,6 @@ const scroll = new LocomotiveScroll({
 });
 //pour changer le body color//
 scroll.on("scroll", () => {
-    console.log("ca scroll!!");
     if (document.querySelector("#color.is-inview")) {
         document.body.style.background = "#000101";
         document.body.style.color = "#fefeff";
